5c27f376f9f754d3b66b365c81a92bfe96f56815
Pr@ndeep173 <PP@173>
2022-09-29 18:53:37 +0530

    My commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'My-Title';
+
+  count = 7;
+  number1 = 1;
+  number2 = 2;
+  result = this.number1 + this.number2;
+
+  listofnumbers =  [ "Audi" , "BMW" , "Lotus" ];
+
+  decrement = () => {
+    this.count--;
+  }
 }
